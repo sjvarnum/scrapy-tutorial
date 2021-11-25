1439b8797df89f5d94c57b2914909ec153db0f24
Shane Varnum <shane.varnum@encompassmedia.dev>
2021-11-24 19:56:34 -0500

    Implement page crawl

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -5,8 +5,7 @@ import scrapy
 class QuoteSpider(scrapy.Spider):
     name = 'quotes' # indentifies the spider
     start_urls = [
-        'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/',
+        'http://quotes.toscrape.com'
     ]
 
     def parse(self, response):
@@ -16,3 +15,8 @@ class QuoteSpider(scrapy.Spider):
                 'author': quote.css('small.author::text').get(),
                 'tags': quote.css('div.tags a.tag::text').getall(),
             }
+
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
